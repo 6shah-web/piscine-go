813effaa2f17222c5f5f86c6fc5f8ca7f972b798
Shah <shah@Shaials-MacBook-Pro.local>
2022-08-11 22:20:17 +0100

    is negative

--- isnegative.go
+++ isnegative.go
@@ -1,16 +1,19 @@
 package main
 
-import ("github.com/01-edu/z01")
+import (
+	"github.com/01-edu/z01"
+)
 
 func IsNegative(nb int) {
 
-	if { nb >= 0
-	z01.PrintRune('F')
-	z01.PrintRune('\n')
+	if nb >= 0 {
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
 
 	} else {
+
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
-		
+
 	}
 }
