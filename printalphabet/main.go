32c4f059310873ea60d5e41a9838ff3da572980e
Your Name <you@example.com>
2022-08-11 11:21:54 +0100

    my first go program

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/01-edu/"
+	"github.com/01-edu"
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	
     for ch := 'a'; ch <= 'z'; ch++ {
+		
         fmt.Printf("%c   ", ch)
-		z01.PrintRune ('\n')
-}
-
+		z01.PrintRune ('\n') 
+	
+	}
