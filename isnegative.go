1af375b9c510ac15467177a7f5ab01db495cbcca
Your Name <you@example.com>
2022-08-12 09:48:00 +0100

    is negative

--- isnegative.go
+++ isnegative.go
@@ -5,15 +5,11 @@ import (
 )
 
 func IsNegative(nb int) {
-
-	if nb int >= 0 {
-		z01.PrintRune('F')
-		z01.PrintRune('\n')
-
-	} else {
-
+	if nb < 0 {
 		z01.PrintRune('T')
-		z01.PrintRune('\n')
-
+	} else {
+		z01.PrintRune('F')
 	}
+
+	z01.PrintRune('\n')
 }
