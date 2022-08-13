e3177cd4253a1e2c248cf30c4ec36d93d1251dda
6shah <6shah@noreply.learn.01founders.co>
2022-08-13 20:26:51 +0000

    Update 'isnegative.go'

--- isnegative.go
+++ isnegative.go
@@ -4,14 +4,14 @@ import "github.com/01-edu/z01"
 
 func Isnegative(nb int) {
 
-	if nb < 0 {
+	if nb >= 0 {
 
-		z01.PrintRune('T')
+		z01.PrintRune('F')
 		z01.PrintRune('\n')
 
 	}else {
 
-		z01.PrintRune('F')
+		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	}
 }
