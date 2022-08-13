dd4925439bb97c497e39c88274da1b43e461ca9a
6shah <6shah@noreply.learn.01founders.co>
2022-08-13 21:15:06 +0000

    is negative

--- isnegative.go
+++ isnegative.go
@@ -7,9 +7,11 @@ func IsNegative(nb int) {
     if nb <= 0 {
 
         z01.PrintRune('T')
+        z01.PrintRune('\n')
         
     }else {
 
         z01.PrintRune('F')
+        z01.PrintRune('\n')
     }
 }
