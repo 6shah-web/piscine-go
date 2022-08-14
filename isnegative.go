d398fe5cdbf5b0a3eeeae7120214c6b5c88d4756
Your Name <you@example.com>
2022-08-14 15:39:55 +0100

    is negative

--- isnegative.go
+++ isnegative.go
@@ -3,15 +3,14 @@ package piscine
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
+	if nb <= 0 {
 
-    if nb <= 0 {
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
 
-        z01.PrintRune('T')
-        z01.PrintRune('\n')
-        
-    }else {
+	} else {
 
-        z01.PrintRune('F')
-        z01.PrintRune('\n')
-    }
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
+	}
 }
